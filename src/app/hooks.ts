bb7de5966b85b75752db6a4e99fb9ad08a2ee572
Ali Hamidi <allii09300hamidi@gmail.com>
2025-04-11 20:59:27 +0430

    create type hooks useAppDispatch and useAppSelector

---
+++ src/app/hooks.ts
@@ -0,0 +1,13 @@
+import {
+  UseDispatch,
+  useSelector,
+  TypedUseSelectorHook,
+  useDispatch,
+} from "react-redux";
+import { AppDispatch, RootState } from "./store";
+
+export const useAppDispatch = () => {
+  useDispatch<AppDispatch>();
+};
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
